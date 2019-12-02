ee3e5ecb094054aba0b87e0a8529c673940349f1
Zahan Malkani <zahanm@fb.com>
2019-12-01 20:25:56 -0800

    Initial Commit

---
+++ Movie Night/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movie Night
+//
+//  Created by Zahan Malkani on 12/1/19.
+//  Copyright © 2019 Wildlings. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
