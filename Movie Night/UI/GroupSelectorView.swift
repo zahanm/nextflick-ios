360cf31c0692b5ad0f1f6f4cc10ff9076a2e465f
Zahan Malkani <zahanm@fb.com>
2019-12-20 17:59:35 +0530

    size the group avatar container appropriately

--- Movie Night/UI/GroupSelectorView.swift
+++ Movie Night/UI/GroupSelectorView.swift
@@ -15,14 +15,17 @@ struct GroupSelectorView: View {
     var body: some View {
         let d = CGFloat(100)
         let w = d * (1.0 - overlap)
-        return HStack(spacing: 0) {
-            CircularAvatarView(person: "Zahan", diameter: d)
-                .frame(width: w)
-            CircularAvatarView(person: "Vibha", diameter: d)
-                .frame(width: w)
-            CircularAvatarView(person: "Ollie", diameter: d)
-                .frame(width: w)
-        }
+        return
+            HStack(spacing: 0) {
+                CircularAvatarView(person: "Zahan", diameter: d)
+                    .frame(width: w)
+                CircularAvatarView(person: "Vibha", diameter: d)
+                    .frame(width: w)
+                CircularAvatarView(person: "Ollie", diameter: d)
+                    .frame(width: w)
+            }
+            .padding([.horizontal], d * overlap / 2)
+
     }
 }
 
