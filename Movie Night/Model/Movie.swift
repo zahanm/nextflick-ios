ab2166e5d2123db208045cfdff289d91e8943dbe
Zahan Malkani <zahanm@fb.com>
2019-12-22 15:32:15 +0530

    re-organise into model, view, and [controller]

---
+++ Movie Night/Model/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  Movie Night
+//
+//  Created by Zahan Malkani on 12/22/19.
+//  Copyright © 2019 Wildlings. All rights reserved.
+//
+
+struct Movie : Identifiable {
+    let id : Int
+    let name : String
+    let image : String
+}
