35cb0c5b3d7d855913c52d7b06889db692fc863d
Zahan Malkani <zahanm@fb.com>
2019-12-22 13:08:36 +0530

    posters grid view

---
+++ Movie Night/UI/PosterGridScrollView.swift
@@ -0,0 +1,55 @@
+//
+//  PosterGridScrollView.swift
+//  Movie Night
+//
+//  Created by Zahan Malkani on 12/22/19.
+//  Copyright © 2019 Wildlings. All rights reserved.
+//
+
+import SwiftUI
+
+struct PosterGridScrollView: View {
+    let posters = [
+        Poster(id: 0, imageName: "joker"),
+        Poster(id: 1, imageName: "favourite"),
+        Poster(id: 2, imageName: "ford-v-ferrari"),
+        Poster(id: 3, imageName: "green-book"),
+    ]
+
+    var body: some View {
+        var grouped: [(Poster, Poster?)] = []
+        for i in stride(from: 0, to: posters.count, by: 2) {
+            var second: Poster?
+            if i + 1 < posters.count {
+                second = posters[i + 1]
+            } else {
+                second = nil
+            }
+            grouped.append((posters[i], second))
+        }
+
+        return ScrollView {
+            VStack(spacing: 0) {
+                ForEach(grouped, id: \.0.id) { row in
+                    HStack(spacing: 0) {
+                        Image(row.0.imageName).border(Color.gray)
+                        if row.1 != nil {
+                            Image(row.1!.imageName).border(Color.gray)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct Poster : Identifiable {
+    let id : Int
+    let imageName : String
+}
+
+struct PosterGridScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        PosterGridScrollView()
+    }
+}
