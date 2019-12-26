77a532eb6136a9b98320cdb4dce6a1cb284a7612
Zahan Malkani <zahanm@fb.com>
2019-12-26 18:08:23 +0530

    re-organize SceneDelegate a bit

--- Movie Night/SceneDelegate.swift
+++ Movie Night/SceneDelegate.swift
@@ -17,14 +17,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
-        // Create the SwiftUI view that provides the window contents.
-        let movies = TmdbAPI.mockMovies()
-        let contentView = MovieListView(movies: movies)
-
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
+
+            // Create the SwiftUI view that provides the window contents.
+            let movies = TmdbAPI.mockMovies()
+            let contentView = MovieListView(movies: movies)
+            window.rootViewController = UIHostingController(
+                rootView: contentView
+            )
+
             self.window = window
             window.makeKeyAndVisible()
         }
